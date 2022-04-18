7aacd61cffdc0fcbb3898daa3156650aa584f831
Tarik Besic <tarik_besic@live.com>
2022-04-18 14:42:10 +0200

    wip

--- convert.php
+++ convert.php
@@ -1,31 +1,27 @@
 <?php
 include "konekcija.php";
-    $sql = "SELECT * FROM `users` WHERE `id`='500'";
-    $id;
-    $ime;
-    $email;
-    $money;
+    $sql = "SELECT * FROM `kurs` WHERE `id`='500'";
+    $eur;
+    $vrijednost;
     $result = $konekcija->query($sql);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            $id = $row['id'];
-            $ime = $row['ime'];
-            $email = $row['email'];
-            $money = $row['novac'];
+            $eur = $row['eur'];
+            $vrijednost = $row['vrijednost'];
         }
-
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/user/dashboard.css">
     <title>Document</title>
 </head>
 <body>
-    <div class="sidebar">
+    
+<div class="sidebar">
         <div class="upper">
             <div class="hamburger">
                 <img src="./images/menu.png" class="img">
@@ -51,14 +47,12 @@ include "konekcija.php";
         </a>
     </div>
     <div class="header">
-        <h1>DOBRO DOSAO <?php echo $name;?> </h1>
+        <h1>DOBRO DOSAO DA ZAMIJENIS VALUTE </h1>
     </div>
 
-    <div class="container">
-        <div class="card">
-            Trenutno imas <?php echo $money?> KM;
-            <!-- slikica neka -->
-        </div>
+    <div class="content">
+        TRENUTNI KURS <?php echo $eur?> JE <?php echo $vrijednost?>
+        <input type="text" id="money">
     </div>
 </body>
 </html>
